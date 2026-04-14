c1e862f53ffa69323fad004922144a9b68eeb645
ThereWasAYang <849916464@qq.com>
2026-04-14 23:54:47 +0800

    Fix nrLDPCEncode sionna path for BG2 K=640 case | Add test file

--- tests/test_nrLDPCEncode.py
+++ tests/test_nrLDPCEncode.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pytest
 from py3gpp.nrLDPCEncode import nrLDPCEncode
+from py3gpp.nrCodeBlockSegmentLDPC import nrCodeBlockSegmentLDPC
+from py3gpp.nrDLSCHInfo import nrDLSCHInfo
+from py3gpp.nrCRCEncode import nrCRCEncode
 
 @pytest.mark.parametrize("K", [2560])
 @pytest.mark.parametrize("C", [1, 2, 3])
@@ -14,9 +17,29 @@ def test_nrLDPCEncode(K, C, F, bgn):
     codedcbs_2 = nrLDPCEncode(cbs.copy(), bgn, algo = 'thangaraj')
     assert np.array_equal(codedcbs_1, codedcbs_2)
 
+def test_nrLDPCEncode_sionna_bg2_k640_valid_case():
+    tbs = 552
+    R = 120 / 1024
+
+    info = nrDLSCHInfo(tbs, R)
+    assert info["BGN"] == 2
+    assert info["K"] == 640
+    assert info["Zc"] == 64
+    assert info["C"] == 1
+
+    rng = np.random.default_rng(7)
+    tb = rng.integers(0, 2, size=tbs, dtype=np.int8)
+    tb_crc = nrCRCEncode(tb, info["CRC"])[:, 0].astype(np.int8)
+    cbs = nrCodeBlockSegmentLDPC(tb_crc, info["BGN"])
+
+    encoded = nrLDPCEncode(cbs, info["BGN"], algo="sionna")
+
+    assert encoded.shape == (info["N"], info["C"])
+
 if __name__ == '__main__':
     bgn = 2
     C = 2
     K = 2560
     F = 36
     test_nrLDPCEncode(K, C, F, bgn)
+    test_nrLDPCEncode_sionna_bg2_k640_valid_case()
